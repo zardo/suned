613e3efe80db89a0e931a5b0b58098703773645c
LUCAS ZARDO <lucas.zardo@caixa.gov.br>
2018-04-25 10:25:38 -0300

    Commit inicial

---
+++ src/main/java/br/gov/caixa/bsb/suned/Application.java
@@ -0,0 +1,13 @@
+package br.gov.caixa.bsb.suned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+	
+}
